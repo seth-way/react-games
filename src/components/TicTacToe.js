733bf990f3c0d72ded797583dbdac60a2bf27789
Seth Way <way.sethaniel@gmail.com>
2024-08-11 14:49:55 -0400

    add game ui buttons

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -7,6 +7,8 @@ export default function TicTacToe() {
   const [history, setHistory] = useState([Array(9).fill(0)]);
   const [winning3, setWinners] = useState([]);
   const [winner, declareWinner] = useState(null);
+  const [playerXWins, setXWins] = useState(0);
+  const [playerYWins, setYWins] = useState(0);
 
   const currentSquares = history[history.length - 1];
   const playersTurn = history.length % 2 ? "X" : "O";
@@ -32,27 +34,46 @@ export default function TicTacToe() {
     const nextSquares = [...currentSquares];
     nextSquares[i] = history.length % 2 ? 1 : -1;
     const winningSquares = checkBoard(nextSquares);
-    if (winningSquares) {
-      setWinners(winningSquares);
-      const winningPlayer = nextSquares[winningSquares[0]] > 0 ? "X" : "O";
-      declareWinner(winningPlayer);
-    }
+    if (winningSquares) handleWinner(winningSquares, nextSquares);
     setHistory([...history, nextSquares]);
+  }
+
+  function handleWinner(indices, board) {
+    const winningPlayer = board[indices[0]] > 0 ? "X" : "O";
+    setWinners(indices);
+    declareWinner(winningPlayer);
+    if (winningPlayer === "X") setXWins(playerXWins + 1);
+    else setYWins(playerYWins + 1);
+  }
+
+  function clearBoard() {
+    setHistory([Array(9).fill(0)]);
+  }
+
+  function newGame() {
+    clearBoard();
+    setWinners([]);
+    declareWinner(null);
   }
 
   return (
     <div id="tic-tac-toe-game">
       <h2>{createStatusMessage()}</h2>
-      <div>
-        <ScoreBoard player={"X"} wins={15} />
-        <Board
-          squares={currentSquares}
-          onPlay={handlePlay}
-          winning3={winning3}
-          isComplete={winner || isDraw}
-          playersTurn={playersTurn}
-        />
-        <ScoreBoard player={"O"} wins={3} />
+
+      <Board
+        squares={currentSquares}
+        onPlay={handlePlay}
+        winning3={winning3}
+        isComplete={winner || isDraw}
+        playersTurn={playersTurn}
+      />
+      <div className="game-info">
+        <ScoreBoard player="X" wins={playerXWins} />
+        <ScoreBoard player="O" wins={playerYWins} />
+      </div>
+      <div className="buttons">
+        <button>clear board</button>
+        <button>new game</button>
       </div>
     </div>
   );
--- src/components/ticTacToe/ScoreBoard.js
+++ src/components/ticTacToe/ScoreBoard.js
@@ -1,13 +1,18 @@
 export default function ScoreBoard({ player, wins }) {
-  const winCount = Array(wins).fill(0);
   return (
     <div className="scoreboard">
       <h3 className={`player-${player}`}>{player}</h3>
-      <ol className="wins">
-        {winCount.map(() => (
-          <li></li>
-        ))}
-      </ol>
+      {wins ? (
+        <ol className="wins">
+          {Array(wins)
+            .fill(0)
+            .map(() => (
+              <li></li>
+            ))}
+        </ol>
+      ) : (
+        "0"
+      )}
     </div>
   );
 }
